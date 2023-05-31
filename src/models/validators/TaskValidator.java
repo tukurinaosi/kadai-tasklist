9b045962e3032355546c3ec1b46db489149030f7
user082.dounyu <2023dounyukenshu.082@gmail.com>
2023-05-31 10:53:32 +0900

    komitto

---
+++ src/models/validators/TaskValidator.java
@@ -0,0 +1,39 @@
+package models.validators;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import models.Task;
+
+public class TaskValidator {
+    // バリデーションを実行する
+    public static List<String> validate(Task t) {
+        List<String> errors = new ArrayList<String>();
+
+
+        String content_error = validateContent(t.getContent());
+        if(!content_error.equals("")) {
+            errors.add(content_error);
+        }
+
+        return errors;
+    }
+
+    // タイトルの必須入力チェック
+    private static String validateTitle(String title) {
+        if(title == null || title.equals("")) {
+            return "タイトルを入力してください。";
+        }
+
+        return "";
+    }
+
+    // メッセージの必須入力チェック
+    private static String validateContent(String content) {
+        if(content == null || content.equals("")) {
+            return "メッセージを入力してください。";
+        }
+
+        return "";
+    }
+}
